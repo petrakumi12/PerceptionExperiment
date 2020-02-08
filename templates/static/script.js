11e22bea4be0e1fce59f5dc74f7ddd871f3cde24
Petra Kumi <pkumi@wpi.edu>
2020-02-08 13:27:55 -0500

    help box in index html

--- templates/static/script.js
+++ templates/static/script.js
@@ -1,18 +1,5 @@
 window.onload = function () {
-    db_test()
     console.log(d3)
-}
-
-function db_test() {
-    console.log('here')
-    fetch('/db_test', {
-        method: 'GET',
-    })
-        .then(res => res.json())
-        .then(res => {
-            console.log(res)
-            document.getElementsByTagName('body')[0].innerHTML = '<p>' + res.text + '</p>'
-        })
 }
 
 console.log(d3.randomInt);
