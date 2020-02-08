fb13ea04637f1229344d713015672ff2af5ec979
Philippe <pslessard@gmail.com>
2020-02-08 13:21:38 -0500

    did the shuffling

--- templates/static/script.js
+++ templates/static/script.js
@@ -20,3 +20,24 @@ var data = d3.range(10).map(() => {
 	return d3.randomInt(1, 101)();
 });
 console.log(data);
+
+//var tests = d3.range(10);
+var tests = [
+	() => console.log("Test 1"),
+	() => console.log("Test 2"),
+	() => console.log("Test 3"),
+	() => console.log("Test 4"),
+	() => console.log("Test 5"),
+	() => console.log("Test 6"),
+	() => console.log("Test 7"),
+	() => console.log("Test 8"),
+	() => console.log("Test 9"),
+	() => console.log("Test 10")
+]
+console.log("tests")
+tests.map((f) => f());
+console.log("shuffle")
+d3.shuffle(tests).map((f) => f())
+
+console.log("shuffle")
+d3.shuffle(tests).map((f) => f())
